760c88816b42ecd5a0adb6030536aeb9e3c3270f
葉建廷 <yejianting@yejiantingdeMacBook-Pro.local>
2021-05-18 03:40:05 +0800

    fix config.ini

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 global $db;
-$db["server"] = "localhost";
-$db["user"] = "root";
-$db["password"] = "";
-$db["dbname"] = "order";
+$db["server"] = "us-cdbr-east-03.cleardb.com";
+$db["user"] = "baefa20d632630";
+$db["password"] = "f7cae5a7";
+$db["dbname"] = "heroku_e18fa8eec266c02";
 // Create connection
 $conn = new mysqli($db["server"], $db["user"], $db["password"], $db["dbname"]);
 if ($conn->connect_error) {
